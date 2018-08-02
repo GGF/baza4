5b24e47d23a1b3941bb4ecbc50273eb99291982f
GGF <georgeg.fedoroff@gmail.com>
2018-08-02 14:19:40 +0300

    cleareded
    Подчистил и сделал чтобы там где апплеты, работали апплеты, иначе копирование в буффер.

--- _engine/classes/sqltable/js/sqltable.js
+++ _engine/classes/sqltable/js/sqltable.js
@@ -4,16 +4,25 @@
  *  Используют диалог jquery.dialog
  */
 
+/* global Lang 
+ * Это для локализации, если будет неопределена то и фиг с ней*/
+/* global Noapplet
+ * Если нет апплета, то истина*/
+var Noapplet = false;
+
 /**
  * Идентификатор первой строки в таблице
+ * @type String
  */
 var firsttr = '';
 /**
  * Идентификатор текущей строки в таблице
+ * @type String
  */
 var curtr = firsttr;
 /**
  * Идетификатор последней строки
+ * @type String
  */
 var lasttr = '';
 
@@ -26,9 +35,23 @@ var lasttr = '';
 /**
  * Объект для копирования в буффер данных, должен быть глобальным, потому как меняется в разных местах.
  * 
- * @type Object
+ * @type ClipboardJS
  */
 var filelinkclipboard;
+/**
+ *  @type ClipboardJS
+ */
+var pathlinkclipboard;
+/**
+ *  @type ClipboardJS
+ */
+var printlinkclipboard;
+/**
+ * Объект для копирования в буффер данных в формате excel.
+ * 
+ * @type ClipboardJS
+ */
+var copytoexcell;
 
 
 /**
@@ -45,7 +68,7 @@ function reload_table() {
 
 /**
  * Вызов диалога с текстом
- * @param boolean info инфформационное окно (Ok) или  с формой (ok=cancel)
+ * @param info boolean инфформационное окно (Ok) или  с формой (ok=cancel)
  */
 function dialog_modal(info)
 {
@@ -122,8 +145,12 @@ function dialog_modal(info)
         $('.showcomment').hide();
     }
     
-    // а вот тут диалог уже есть и можно поменять контейнер в filelinkclipboard
-    filelinkclipboard.container = document.getElementById("dialog");
+    if (Noapplet) {
+        // а вот тут диалог уже есть и можно поменять контейнер в filelinkclipboard
+        filelinkclipboard.container = document.getElementById("dialog");
+        pathlinkclipboard.container = document.getElementById("dialog");
+        printlinkclipboard.container = document.getElementById("dialog");
+    }
 }
 
 // Focus first element
@@ -180,69 +207,134 @@ $(document).ready(function(){
 
     // Назначим клавиатурные комбинации
     table_set_keyboard();
-    /* следующий блок добавляет контекстное меню для копирования ссылок
-	   и для открытия по клику использует applet
-	*/
-//    $(document).contextMenu( function () {
-//        return CMenu_builder(this.event);
-//    });
-
-    // при клике на файловых ссылках вызовем из небезопасно, зато удобно из командного процессора
-    $(document).on("click","a.filelink", function(){
-        var re = new RegExp('/','gi');
-        var link = $(this).attr("href").split(':')[1].replace(re,'\\');
-        document.bazaapplet.openfile('\"'+link+'\"');
-        return false;
-    });
-    // добавим копирование команды в буффер с помощью clipboard.js
-    // Глобальная переменная
-    filelinkclipboard = new ClipboardJS('a.filelink', {
-        text: function(trigger) {
-            var re = new RegExp('/','gi');
-            var link = trigger.href.split(':')[1].replace(re,'\\');
-            return 'mppapp-commandOPENFILE'+'\"'+link+'\"';
-        } 
-      // , container: document.getElementById('dialog') // В этот момент диалога еще нет
-    });
-
-    filelinkclipboard.on('success', function(e) {
-        console.log("filelinkclipboard success");
-        console.log(e);
-    });
     
-    filelinkclipboard.on('error', function(e) {
-        console.log("filelinkclipboard error");
-        console.log(e);
-    });
-    
-    // Ссылки "печатать"
-    $(document).on("click","a.printlink", function(){
-        var link = $(this).attr("href");
-            var re = new RegExp('/','gi');
-            // программа лежит в аксессуарах, скопирована с http://www.robvanderwoude.com/csharpexamples.php#PrintAny
-            document.bazaapplet.openfile('printany.exe \"'+link.split(':')[1].replace(re,'\\')+'\"');
-            return false;
-    });
-    // при клике на ссылках "путь" соответственно запустим программу открытия пути
-    $(document).on("click", "a.path", function(){
-        var link = $(this).attr("href");
-            var re = new RegExp('/','gi');
-            // а программа хранится в локальном хранилище, и довольно долго
-            var totalcmd = localStorage.getItem('total_cmd_path'); // 'd:\\Total Commander XP\\TOTALCMD.EXE';
+    // прверка на апплет
+    try {
+            document.bazaapplet.copytoclipboard('');
+        } catch (exception) {
+            console.log(exception);
+            Noapplet = true;
+    }
 
-            if (totalcmd === null) {
-                // если не хранится пошлем настраивать
-                alert(Lang.get('sqltable.warnings.nototalfind'));
-                $(this).attr("href","/?level=setting");
-                return true;
+    if (Noapplet) {
+        // добавим копирование команды в буффер с помощью clipboard.js
+        // Глобальная переменная
+        filelinkclipboard = new ClipboardJS('a.filelink', {
+            text: function(trigger) {
+                var re = new RegExp('/','gi');
+                var link = trigger.href.split(':')[1].replace(re,'\\');
+                return 'mppapp-commandOPENFILE'+'\"\"'+decodeURIComponent(link)+'\"\"';
+            } 
+          // , container: document.getElementById('dialog') // В этот момент диалога еще нет
+        });
+
+        printlinkclipboard = new ClipboardJS('a.printlink', {
+            text : (trigger) => {
+                var re = new RegExp('/','gi');
+                var link = trigger.href.split(':')[1].replace(re,'\\');
+                return 'mppapp-commandOPENFILE'+'printany.exe \"\"'+decodeURI(link)+'\"\"';
             }
-            //alert('\"mkdir \"'+link+'\"\"');
-            //document.bazaapplet.openfile('\"mkdir \"'+link+'\"\"');
-            var res = document.bazaapplet.openfile('\"\"'+totalcmd+'\" \"'+link+'\"\"');
-            $().lego.log(res);
-            return false; // чтоб нажатие на ссылку броузер не отработал нормальным способом
-    });
+        });
+    
+        pathlinkclipboard = new ClipboardJS('a.path',{
+            text : (trigger) => {
+                // а программа хранится в локальном хранилище, и довольно долго
+                var totalcmd = localStorage.getItem('total_cmd_path'); // 'd:\\Total Commander XP\\TOTALCMD.EXE';
+                if (totalcmd === null) {
+                    // если не хранится пошлем настраивать
+                    alert(Lang.get('sqltable.warnings.nototalfind'));
+                    $(this).attr("href","/?level=setting");
+                    return true;
+                }
+                return 'mppapp-commandOPENFILE'+'\"\"'+totalcmd+'\" \"'+decodeURI(trigger.href)+'\"\"';
+            }
+        });
+        // копирование по кнопке в эксель
+        copytoexcell = new ClipboardJS('#copytablebutton', {
+            text : () => {
+                var str = '';
+                var buffer = '';
+                // для каждой строчки
+                $('table.listtable>tbody>tr').each(function(){
+                    $(this).find('td').each(function(){
+                        if ( str === '') {
+                            // для пустой добавляем
+                            str = $(this).text() ;
+                        } else {
+                            // для не пустой - разделитель и добавляем
+                            str = str + "\t" + $(this).text() ;
+                        }
+                    });
+                    // и в буфер все, в буфер
+                    buffer = buffer + str + "\n";
+                    str = '';
+                });
+                return buffer;
+            }
+        });
+        copytoexcell.on('success',(target)=>{
+            target.trigger.style.display='none';
+        });
+    } else {
+        // при клике на файловых ссылках вызовем из небезопасно, зато удобно из командного процессора
+        $(document).on("click","a.filelink", function(){
+            var re = new RegExp('/','gi');
+            var link = $(this).attr("href").split(':')[1].replace(re,'\\');
+            document.bazaapplet.openfile('\"'+link+'\"');
+            return false;
+        });
+        // Ссылки "печатать"
+        $(document).on("click","a.printlink", function(){
+            var link = $(this).attr("href");
+                var re = new RegExp('/','gi');
+                // программа лежит в аксессуарах, скопирована с http://www.robvanderwoude.com/csharpexamples.php#PrintAny
+                document.bazaapplet.openfile('printany.exe \"'+link.split(':')[1].replace(re,'\\')+'\"');
+                return false;
+        });
+        // при клике на ссылках "путь" соответственно запустим программу открытия пути
+        $(document).on("click", "a.path", function(){
+                var link = $(this).attr("href");
+                var re = new RegExp('/','gi');
+                // а программа хранится в локальном хранилище, и довольно долго
+                var totalcmd = localStorage.getItem('total_cmd_path'); // 'd:\\Total Commander XP\\TOTALCMD.EXE';
 
+                if (totalcmd === null) {
+                    // если не хранится пошлем настраивать
+                    alert(Lang.get('sqltable.warnings.nototalfind'));
+                    $(this).attr("href","/?level=setting");
+                    return true;
+                }
+                var res = document.bazaapplet.openfile('\"\"'+totalcmd+'\" \"'+link+'\"\"');
+                $().lego.log(res);
+                return false; // чтоб нажатие на ссылку броузер не отработал нормальным способом
+        });
+        
+        // copytablebutton
+        $(document).on('click','#coptablebutton',()=>{
+            var str = '';
+            var buffer = '';
+            // для каждой строчки
+            $('table.listtable>tbody>tr').each(function(){
+                $(this).find('td').each(function(){
+                    if ( str === '') {
+                        // для пустой добавляем
+                        str = $(this).text() ;
+                    } else {
+                        // для не пустой - разделитель и добавляем
+                        str = str + "\t" + $(this).text() ;
+                    }
+                });
+                // и в буфер все, в буфер
+                buffer = buffer + str + "\n";
+                str = '';
+            });
+            // скормим буфер апплету
+            document.bazaapplet.copytoclipboard(buffer);
+            // надпись на кнопке поменяем
+            $('#copytablebutton').hide();
+            return false;
+        });
+    }
 });
 
 function table_set_keyboard()
@@ -265,8 +357,8 @@ function table_set_keyboard()
 
         // все буквы и цифры передаем ввод в строку поиска
         // правда исключаются символы типа двоеточия
-        if ($.inArray(event.keyCode,$.keyb.getRange('letters')) != -1 ||
-            $.inArray(event.keyCode,$.keyb.getRange('allnum')) != -1) {
+        if ($.inArray(event.keyCode,$.keyb.getRange('letters')) !== -1 ||
+            $.inArray(event.keyCode,$.keyb.getRange('allnum')) !== -1) {
             // для ввода в строку поиска
             // если в поиске уже чтото есть
             //if ($('#dialog').length>0 && $('#dialog').is(':visible')) {
@@ -276,7 +368,7 @@ function table_set_keyboard()
             } else {
                 // иначе найдем строку поиска
                 var find=$('input[name=find]');
-                if (find.val().length==0) {
+                if (find.val().length===0) {
                     // и кинем туда фокус ввода
                     find.focus();
                     return true;
@@ -286,7 +378,7 @@ function table_set_keyboard()
                 }
             }
             return true;
-        } else if (event.keyCode==$.keyb.getIndexCode('enter')) {
+        } else if (event.keyCode===$.keyb.getIndexCode('enter')) {
             // если ентер
             // то проверяем в поиске и мы
             if ($('input.find').last().val().length>0) {
@@ -318,7 +410,7 @@ function table_set_keyboard()
                 */
             }
             return true;
-        } else if (event.keyCode==$.keyb.getIndexCode('aup')) {
+        } else if (event.keyCode===$.keyb.getIndexCode('aup')) {
             // клавиши верх-низ
             if ($('#dialog').length<=0 || $('#dialog').is(':hidden')) {
                 // желтим-нежелтим
@@ -328,7 +420,7 @@ function table_set_keyboard()
                 $('#'+curtr).addClass("yellow");
                 // и проверяем скролинг
                 if (($('#'+curtr).position().top)<($('#maindiv').position().top)) {
-                    if (curtr == firsttr) {
+                    if (curtr === firsttr) {
                         $('#maindiv').scrollTop(0);
                     } else {
                         $('#maindiv').scrollTop($('#maindiv').scrollTop()-$('#'+curtr).height());
@@ -336,7 +428,7 @@ function table_set_keyboard()
                 }
             }
             return false;
-        } else if (event.keyCode==$.keyb.getIndexCode('adown')) {
+        } else if (event.keyCode===$.keyb.getIndexCode('adown')) {
             // клавиши верх-низ
             if ($('#dialog').length<=0 || $('#dialog').is(':hidden')) {
                 // желтим-нежелтим
@@ -363,78 +455,4 @@ function table_set_keyboard()
 
     });
 
-}
-
-/**
- * Функция срисована с когото, мне нужна была только для пункта меню "Копировать в буффер"
- * использует библиотеку jquery.contextmenu
- */
-function CMenu_builder(oEvent) {
-    var objMenu = [];
-    switch (true) {
-        /* тут даже можно вставить проверку, чтобы целевой фрагмент не был выделенным текстом,
-         * который мы, например, можем хотеть скопировать/вставить при помощи основного меню :-)*/
-        case (document.getSelection().length > 0) :
-            break;
-        case oEvent.target.nodeName === 'A' && oEvent.target.className.search('filelink')>-1 :
-            /* генерируем данные для одного случая в массив objMenu */
-            objMenu.push({
-                'Скопировать в буффер' : function () {
-                    document.bazaapplet.copytoclipboard(Url.decode(oEvent.target.href));
-                    return true;
-                }
-            });
-            break;
-        case oEvent.target.nodeName === 'BUTTON' && oEvent.target.className.search('subElems')>-1 :
-            /* генерируем данные для другого случая objMenu */
-            break;
-        case oEvent.target.nodeName === 'a' && oEvent.target.parentNode.id ==='footer' :
-            /* генерируем данные для другого случая objMenu из статичного набора*/
-            objMenu.push({
-                'Действие №1' : function () {
-                    return true;
-                }
-            });
-            objMenu.push({
-                'Действие №2' : function () {
-                    return true;
-                }
-            });
-            //etc
-            break;
-        default:
-            return false;
-            break;
-    }
-    return objMenu.length ? objMenu : false;
-}
-
-/**
- * Это замечательная штука использует копирование в буффер апплета
- * и загоняет туда таблицу в формате удобном для вставки
- * в excel, а  то ссылки вставлялись и другое оформление
- */
-function copytable() {
-    var str = '';
-    var buffer = '';
-    // для каждой строчки
-    $('table.listtable>tbody>tr').each(function(){
-        $(this).find('td').each(function(){
-            if ( str === '') {
-                // для пустой добавляем
-                str = $(this).text() ;
-            } else {
-                // для не пустой - разделитель и добавляем
-                str = str + "\t" + $(this).text() ;
-            }
-        });
-        // и в буфер все, в буфер
-        buffer = buffer + str + "\n";
-        str = '';
-    });
-    // скормим буфер апплету
-    document.bazaapplet.copytoclipboard(buffer);
-    // надпись на кнопке поменяем
-    $('#copytable').val('Готово');
-    return false;
 }
--- classes/firstlevel/view/help.tpl
+++ classes/firstlevel/view/help.tpl
@@ -1,17 +1,31 @@
   Для того чтобы запускать файлы эксель по ссылкам нужно установить программу 
   <a href="nncron193b3.exe"> nncron.</a>
   
-Создать в ней задачу c именем OpenInExcel
+Создать в ней задачу c именем clipboard
 и текстом
 
-  <pre>WatchClipboard: "*"
-: wget1-mask S" /file:\/\/.+\.(xls)|(xml)/i" ;
-Rule: WIN-ACTIVE: "База данных ЗАО МПП*" RE-MATCH: %CLIPBOARD% %wget1-mask% AND
+  <pre>#( clipboard_command
+NoLog
+\ NoActive
+WatchClipboard: "mppapp-command*"
 Action:
-QUERY: "Открыть в Excel?"
+CLIPBOARD 
+14 /STRING \ обезать ключ из строки
+2DUP \ продублировать строку
+S" OPENFILE" \ строка для сравнгения
+StringGetPos \ найти вторую в первой
+0<> \ нашли строку
 IF
-ShowNormal   NormalPriority
-START-APP: explorer %CLIPBOARD%
+	8 /STRING \ отрезать команду
+	2DUP
+	TYPE CR \ показать
+	START-APP \ запустить
+ELSE
+	TYPE CR
 THEN
+\ дальше почистить клипбоард
+S" "
+CLIPBOARD!
+)#
 </pre>
-  после этого можно копировать ссылку в буффер и она откроется!!!
+
