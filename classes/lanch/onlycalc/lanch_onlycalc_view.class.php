e20decf1db21d605956b4a641cbe9a97e3a60a1f
ggf <georgeg.fedoroff@gmail.com>
2019-10-18 12:03:59 +0300

    view nzap change

---
+++ classes/lanch/onlycalc/lanch_onlycalc_view.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class lanch_onlycalc_view extends lanch_nzap_view
+{
+  // обязательно определять для модуля
+  public function getDir()
+  {
+    return __DIR__;
+  }
+
+  /**
+   * Как у родителя, только кнопки нужно вырезать
+   */
+  public function showrec($rec) {
+    return preg_replace("|<input[^>]*>|i","",parent::showrec($rec));
+
+  }
+}
